ed76bc4749468d8434b3a248801d8bf334a786bd
Tahir2022 <abbasovtahir617@gmail.com>
2025-11-01 09:57:08 +0100

    Nested if and elif chain are useful

--- adventure_game.py
+++ adventure_game.py
@@ -6,8 +6,19 @@ answer = input("You are on dirt road, " \
 "Which way would you like to go?").lower()
 
 if answer == "left":
+    answer = input("You come to river, you can walk around it" \
+    "or swim across? Type walk to walk around " \
+    "and swim to swim across: ")
+
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost.")
+    else:
+        print("Not a valid option. You lose.")
 
 elif answer == "right":
-
+    answer = input("You come to a bridge, it looks wobbly," \
+    "do you want to across it or")
 else:
     print("Not valid option. You lose.")
