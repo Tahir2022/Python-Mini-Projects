2d80831a49cc3c30dc938f2260c7a8aff8420122
Tahir2022 <abbasovtahir617@gmail.com>
2025-11-03 18:25:31 +0100

    Finished it

--- password_generator.py
+++ password_generator.py
@@ -16,9 +16,24 @@ def generate_password(min_length, numbers = True, special_characters = True):
     meets_criteria = False
     has_number = False
     has_speacial = False
-'''
+
     while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
 
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_speacial = True
 
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_speacial
 
-generate_password(10)'''
+    return pwd
+
+min_length = int(input("Enter the minimum length: "))
+pwd = generate_password(10) 
+print(pwd)
